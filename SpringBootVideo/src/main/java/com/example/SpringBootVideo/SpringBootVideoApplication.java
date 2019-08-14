cfdab34eaef17aa7d7206948a25a0e1696ced91f
ZL0101 <31301959+ZL0101@users.noreply.github.com>
2019-08-14 22:38:25 +0800

    zl3

--- SpringBootVideo/src/main/java/com/example/SpringBootVideo/SpringBootVideoApplication.java
+++ SpringBootVideo/src/main/java/com/example/SpringBootVideo/SpringBootVideoApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootVideoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("here");
 		SpringApplication.run(SpringBootVideoApplication.class, args);
 	}
 
